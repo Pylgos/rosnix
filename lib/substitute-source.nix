3241d3d5eed5bc11a92c1dca3b4abd6ba3e47136
Pylgos <43234674+Pylgos@users.noreply.github.com>
2024-09-23 14:31:27 +0900

    substituteSource: add tar option

--- lib/substitute-source.nix
+++ lib/substitute-source.nix
@@ -1,34 +1,57 @@
 { lib, stdenvNoCC }:
 
-{ src, substitutions }:
-if substitutions == [ ] then
-  src
-else
-  stdenvNoCC.mkDerivation {
-    name = "${src.name}-substituted";
-    src = src;
-    phases = [
-      "unpackPhase"
-      "patchPhase"
-      "installPhase"
-    ];
-    patchPhase =
-      let
-        genLine =
-          subst:
-          "substituteInPlace ${
-            lib.escapeShellArgs [
-              subst.path
-              "--replace-fail"
-              subst.from
-              subst.to
-            ]
-          }\n";
-        s = lib.concatStrings (map genLine substitutions);
-      in
-      s;
-    installPhase = ''
-      cp -a . $out
-    '';
-    preferLocalBuild = true;
-  }
+let
+  self = lib.makeOverridable (
+    {
+      src,
+      substitutions ? [ ],
+      tar ? false,
+    }:
+    if substitutions == [ ] && tar == false then
+      src
+    else
+      stdenvNoCC.mkDerivation {
+        name = "${src.name}-substituted${if tar then ".tar" else ""}";
+        src = src;
+        phases = [
+          "unpackPhase"
+          "patchPhase"
+          "installPhase"
+        ];
+        patchPhase =
+          let
+            genLine =
+              subst:
+              "substituteInPlace ${
+                lib.escapeShellArgs [
+                  subst.path
+                  "--replace-fail"
+                  subst.from
+                  subst.to
+                ]
+              }\n";
+            s = lib.concatStrings (map genLine substitutions);
+          in
+          s;
+        installPhase =
+          if tar then
+            ''
+              pushd ..
+              tar \
+                --create \
+                --sort=name \
+                --format=gnu \
+                --owner=0 --group=0 --numeric-owner \
+                --mtime="@$SOURCE_DATE_EPOCH" \
+                --file "$out" "$sourceRoot"
+              popd
+            ''
+          else
+            ''
+              cp -a . "$out"
+            '';
+        preferLocalBuild = true;
+      }
+  );
+in
+self
