ae474d5b5bae803cb15df263c1a8db43206f567a
Pylgos <43234674+Pylgos@users.noreply.github.com>
2024-09-30 12:26:37 +0900

    scripts/generate.sh: Output report file

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-RUST_BACKTRACE=1 cargo run --manifest-path=generator/Cargo.toml -- --config-file rosnix.toml generate
+RUST_BACKTRACE=1 cargo run --manifest-path=generator/Cargo.toml -- --config-file rosnix.toml generate --report-file generator-report.md && cat generator-report.md
 
